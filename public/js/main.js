61384507f5755f49f06d3f8bb8e69ca56fe70acd
Cindy Wang <cindii.wang@gmail.com>
2019-06-01 12:11:51 -0700

    game start

--- public/js/main.js
+++ public/js/main.js
@@ -74,6 +74,7 @@ socket.on('join_room_response',function(payload){
 	}
 	//if we have seen the person who just joined (soemthing weird happened)
 	else {
+		uninvite(payload.socket_id);
 		var buttonC = makeInviteButton(payload.socket_id);
 		$('.socket_'+payload.socket_id+' button').replaceWith(buttonC);
 		dom_elements.slideDown(1000);
@@ -139,7 +140,7 @@ socket.on('invited',function(payload){
 		return;
 	}
 
-	var newNode = makePlayButton();
+	var newNode = makePlayButton(payload.socket_id);
 	$('.socket_'+payload.socket_id+' button').replaceWith(newNode);
 });
 
@@ -172,22 +173,47 @@ socket.on('uninvited',function(payload){
 	$('.socket_'+payload.socket_id+' button').replaceWith(newNode);
 });
 
-socket.on('send_message_response',function(payload){
+//Send an game_start message to the server
+function game_start(who){
+	var payload = {};
+	payload.requested_user = who;
+	console.log('*** Client Log Message: \'game_start\' payload: '+JSON.stringify(payload));
+	socket.emit('game_start',payload);
+}
+
+//Handle a notification that we have been engaged
+socket.on('game_start_response',function(payload){
 	if(payload.result == 'fail'){
 		alert(payload.message);
 		return;
 	}
-	$('#messages').append('<p><b>'+payload.username+' says:</b> '+payload.message+'</p>');
+
+	var newNode = makeEngagedButton(payload.socket_id);
+	$('.socket_'+payload.socket_id+' button').replaceWith(newNode);
+
+	//Jump to a new page
+	window.location.href = 'game.html?username='+username+'&game_id='+payload.game_id;
 });
 
 function send_message(){
 	var payload = {};
 	payload.room = chat_room;
-	payload.username = username;
 	payload.message = $('#send_message_holder').val();
 	console.log('*** Client Log Message : \'send_message\' payload' + JSON.stringify(payload));
 	socket.emit('send_message',payload);
 }
+
+socket.on('send_message_response',function(payload){
+	if(payload.result == 'fail'){
+		alert(payload.message);
+		return;
+	}
+	var newHTML = '<p><b>'+payload.username+' says:</b> '+payload.message+'</p>';
+	var newNode = $(newHTML);
+	newNode.hide();
+	$('#messages').append(newNode);
+	newNode.slideDown(1000);
+});
 
 //BUTTONS
 
@@ -209,13 +235,16 @@ function makeInvitedButton(socket_id){
 	return(newNode);
 }
 
-function makePlayButton(){
+function makePlayButton(socket_id){
 	var newHTML = '<button type=\'button\' class=\'btn btn-success\'>Play</button>';
 	var newNode = $(newHTML);
+	newNode.click(function(){
+		game_start(socket_id);
+	});
 	return(newNode);
 }
 
-function makeEngageButton(){
+function makeEngagedButton(){
 	var newHTML = '<button type=\'button\' class=\'btn btn-danger\'>Engaged</button>';
 	var newNode = $(newHTML);
 	return(newNode);
