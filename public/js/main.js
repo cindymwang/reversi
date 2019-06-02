a99589236f695ee37b07e14009871ae517bab01a
Cindy Wang <cindii.wang@gmail.com>
2019-06-02 13:10:43 -0700

    game over state

--- public/js/main.js
+++ public/js/main.js
@@ -201,7 +201,7 @@ function send_message(){
 	payload.message = $('#send_message_holder').val();
 	console.log('*** Client Log Message : \'send_message\' payload' + JSON.stringify(payload));
 	socket.emit('send_message',payload);
-	payload.message = $('#send_message_holder').val('');
+	$('#send_message_holder').val('');
 }
 
 socket.on('send_message_response',function(payload){
@@ -257,6 +257,8 @@ $(function(){
 	payload.username = username;
 	console.log('*** Client Log Message : \'join_room\' payload' + JSON.stringify(payload));
 	socket.emit('join_room',payload);
+
+	$('#quit').append('<a href="lobby.html?username='+username+'" class="btn btn-danger btn-default active" role="button" aria-pressed="true">Quit</a>');
 });
 
 
@@ -305,11 +307,19 @@ socket.on('game_update',function(payload){
 		$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
 
 	//animate changes to the board
-
+	var blacksum = 0;
+	var whitesum = 0;
 	var row, column;
 
 	for(row = 0; row < 8; row++){
 		for (column = 0; column < 8; column++){
+			if(board[row][column] == 'b'){
+				blacksum++;
+			}
+			if(board[row][column] == 'w'){
+				whitesum++;
+			}
+
 			//if a board space has changed
 			if(old_board[row][column] != board[row][column]){
 				if(old_board[row][column] == '?' && board[row][column] == ' '){
@@ -363,6 +373,8 @@ socket.on('game_update',function(payload){
 			}
 		}
 	}
+	$('#blacksum').html(blacksum);
+	$('#whitesum').html(whitesum);
 	old_board = board;
 });
 
@@ -374,4 +386,18 @@ socket.on('play_token_response',function(payload){
 		alert(payload.message);
 		return;
 	}
+});
+
+socket.on('game_over',function(payload){
+	console.log('*** Client Log Message: \'game_over\'\n\tpayload: '+JSON.stringify(payload));
+	//check for a good play_token_response
+	if(payload.result == 'fail'){
+		console.log(payload.message);
+		alert(payload.message);
+		return;
+	}
+	//8:28 vid 21
+	$('#game_over').html('<h1>Game Over</h1><h2>'+payload.who_won+' won!</h2>');
+	$('#game_over').append('<a href="lobby.html?username='+username+'" class="btn btn-success btn-lg active" role="button" aria-pressed="true">Return to lobby</a>');
+
 });
